c9041c71fa313e4de65d4bd4fbf821725c549218
Iuryglabson <iuryglabson18@gmail.com>
2022-08-02 17:00:06 -0300

    Feat: Create first component.

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Basket from './src/screens/Basket';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Aluraa!</Text>
+      <Basket/>
       <StatusBar style="auto" />
     </View>
   );
